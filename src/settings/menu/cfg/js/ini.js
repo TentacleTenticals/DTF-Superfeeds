7bb7f096dfe207abd6c158289addb53a0a0fe08c
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-09-27 11:22:03 +0300

    Update ini.js

--- src/settings/menu/cfg/js/ini.js
+++ src/settings/menu/cfg/js/ini.js
@@ -4,19 +4,15 @@ const defaultCfg = {
         'working mode': 'panels'
     },
     'database': {
-        'settings': {
-            'online': true,
-            'db': 'mongoDB'
-        },
-        'data': {
-            'online': true,
-            'db': 'mongoDB'
-        },
-        'keepVars': {
-            'subsites': false,
-            'users': false,
-            'feeds': false,
-            'comments': false
+        'cfg': {
+            'settings': {
+                'online': true,
+                'db': 'mongoDB'
+            },
+            'data': {
+                'online': true,
+                'db': 'mongoDB'
+            },
         },
         'saving': {
             'feeds': {
@@ -90,79 +86,76 @@ const defaultCfg = {
     },
     'feeds': {
         'interface': {
-        'feedButtons': {
-            'readed': true,
-            'author actions': true,
-            'subsite actions': false
-        }
+            'feedButtons': {
+                'readed': true,
+                'author actions': true,
+                'subsite actions': false
+            }
         },
         'what to show': {
-        'popular': {
-            'topics': true,
-            'blogs': true
-        },
-        'new': {
-            'topics': true,
-            'blogs': true
-        },
-        'my new': {
-            'topics': true,
-            'blogs': true
-        },
-        'bookmarks': {
-            'topics': true,
-            'blogs': true
-        },
-        'topic': {
-            'topics': true,
-            'blogs': true
-        }
+            'popular': {
+                'topics': true,
+                'blogs': true
+            },
+            'new': {
+                'topics': true,
+                'blogs': true
+            },
+            'my new': {
+                'topics': true,
+                'blogs': true
+            },
+            'bookmarks': {
+                'topics': true,
+                'blogs': true
+            },
+            'topic': {
+                'topics': true,
+                'blogs': true
+            }
         },
         'attachments': {
-        'video': {
-            'replace': true,
-            'autoplay': false,
-            'sound': false,
-            'volume': 40,
-            'size': {
-            'width': 300,
-            'height': 150
+            'video': {
+                'replace': true,
+                'autoplay': false,
+                'sound': false,
+                'volume': 40,
+                'size': {
+                'width': 300,
+                'height': 150
+                }
             }
-        }
         }
     },
     'usercard': {
-      'avatar': {
-        'search': {
-          'list': [// Список поисковиков.
-            {url:'http://saucenao.com/search.php?db=999&url=', name:'Saucenao', use:true},
-            {url:'https://www.bing.com/images/search?view=detailv2&iss=sbi&FORM=SBIHMP&sbisrc=UrlPaste&q=imgurl:', name:'Bing', use:true},
-            {url:'https://www.google.com/searchbyimage?sbisrc=4chanx&safe=off&image_url=', name:'Google', use:true},
-            {url:'https://lens.google.com/uploadbyurl?url=', name:'Google Lens', use:true},
-            {url:'https://yandex.ru/images/search?rdrnd=296405&rpt=imageview&url=', name:'Yandex', use:true},
-            {url:'http://tineye.com/search/?url=', name:'TinEye', use:true},
-            {url:'http://iqdb.org/?url=', name:'IQDB', use:true}
-          ]
+        'avatar': {
+            'search': {
+                'list': [// Список поисковиков.
+                {url:'http://saucenao.com/search.php?db=999&url=', name:'Saucenao', use:true},
+                {url:'https://www.bing.com/images/search?view=detailv2&iss=sbi&FORM=SBIHMP&sbisrc=UrlPaste&q=imgurl:', name:'Bing', use:true},
+                {url:'https://www.google.com/searchbyimage?sbisrc=4chanx&safe=off&image_url=', name:'Google', use:true},
+                {url:'https://lens.google.com/uploadbyurl?url=', name:'Google Lens', use:true},
+                {url:'https://yandex.ru/images/search?rdrnd=296405&rpt=imageview&url=', name:'Yandex', use:true},
+                {url:'http://tineye.com/search/?url=', name:'TinEye', use:true},
+                {url:'http://iqdb.org/?url=', name:'IQDB', use:true}
+                ]
+            }
         }
-      }
     },
-
-  'emoji picker': {
-      'close after pick': true
-  },
-  'menu': {
-    'feed': {
-        'size': {
-            'max height': '200'
+    'bookMenu': {
+        'visual': {
+            'feeds': {
+                'max height': '500'
+            }
         },
-        'sz': 5
+        'size': {
+            'subsite': 5,
+            'user': 5,
+            'feed': 5
+        }
     },
-    'user': {
-      'sz': 5
+    'scriptInfo': {
+        'name': 'DTF SuperFeeds',
+        'id': 'dtf-superFeeds'
     }
-  },
-  'scriptInfo': {
-      'name': 'DTF SuperFeeds',
-      'id': 'test'
-  }
 }
