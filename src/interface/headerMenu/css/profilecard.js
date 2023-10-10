d652be99fb6a72b7f67c0651ccd755523b804080
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-10-10 14:56:25 +0300

    Create profilecard.js

---
+++ src/interface/headerMenu/css/profilecard.js
@@ -0,0 +1,60 @@
+const profilecardCss = () => `
+.profileCard {
+  display: flex;
+  flex-direction: column;
+  gap: 7px 0;
+  position: absolute;
+  padding: 8px;
+  width: 400px;
+  background-color: rgb(0,0,0);
+  color: rgb(255,255,255);
+  z-index: 1000;
+}
+.profileCard>* {
+  z-index: 1;
+}
+.profileCard>.header {
+  text-align: center;
+}
+
+.profileCard .mask.avatar {
+  width: 150px;
+  aspect-ratio: 1/1;
+  border-radius: 2px;
+  box-shadow: 0 0 3px 0 rgb(255,255,255);
+}
+.profileCard .mask.avatar.zoom {
+  position: absolute;
+  width: 100%;
+}
+.profileCard .mask.cover {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: auto;
+  opacity: 0.2;
+}
+
+.profileCard .list {
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+  gap: 0 5px;
+}
+.profileCard .texter {
+  word-break: break-all;
+  overflow: auto;
+  max-height: 50px;
+  box-shadow: inset 0 0 3px 0px rgb(0,0,0);
+}
+
+.profileCard ul.itemsList {
+  gap: 3px 3px;
+}
+
+.profileCard .off {
+  opacity: 0.5;
+}
+`;
