af701d43cb2fab305253fb53e88208ecf65fc430
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-10-21 16:59:38 +0300

    Update dtfAttachments.js

--- src/func/dtfAttachments.js
+++ src/func/dtfAttachments.js
@@ -1,25 +1,36 @@
 function attachment(o){
-  // console.log(i)
+  console.log('Att', o)
   function player(o){
     new El().Div({
       path: o.path,
+      ...o.addBefore ? {addBefore: o.addBefore} : {},
       cName: 'cont',
       func: (m) => {
         new El().Div({
           path: m,
-          cName: 'video-cont',
-          onclick: (e) => {
-            if(e.button !== 0) return;
-            e.preventDefault();
-            e.stopPropagation();
-            e.stopImmediatePropagation();
-            if(c.lastChild.paused) c.lastChild.play();
-            else c.lastChild.pause();
-          },
+          cName: `${o.type}-cont`,
+          // onclick: (e) => {
+          //   if(e.button !== 0) return;
+          //   e.preventDefault();
+          //   e.stopPropagation();
+          //   e.stopImmediatePropagation();
+          //   if(o.remove) e.target.remove();
+          //   if(m.children[0].lastChild.paused) m.children[0].lastChild.play();
+          //   else m.children[0].lastChild.pause();
+          // },
           func: (p) => {
             new El().Div({
               path: p,
               cName: 'mediaStarter',
+              onclick: (e) => {
+                if(e.button !== 0) return;
+                e.preventDefault();
+                e.stopPropagation();
+                e.stopImmediatePropagation();
+                if(o.remove) e.currentTarget.remove();
+                if(p.lastChild.paused) p.lastChild.play();
+                else p.lastChild.pause();
+              },
               func: (s) => {
                 new El().Div({
                   path: s,
@@ -28,12 +39,13 @@ function attachment(o){
               }
             });
 
-            new El().Video({
+            new El()[o.type === 'video' ? 'Video':'Audio']({
               path: p,
               url: o.url,
               poster: o.poster,
-              loop: true,
-              muted: true,
+              loop: o.type === 'video' ? true:false,
+              muted: o.type === 'video' ? true:false,
+              controls: o.type === 'video' ? false:true,
               onplay: (e) => {
                 e.target.parentNode.classList.toggle('playing');
               },
@@ -48,33 +60,67 @@ function attachment(o){
         });
       }
     });
-    
-    // new El().Image({
-    //   path: prev,
-    //   url: 'https://github.com/TentacleTenticals/dtf-markdown/raw/main/libs/Play.svg'
-    // });
+
+  // new El().Image({
+  //   path: prev,
+  //   url: 'https://github.com/TentacleTenticals/dtf-markdown/raw/main/libs/Play.svg'
+  // });
   }
-  const main=new El().Div({
-    path: o.path,
-    cName: `mask ${o.type} ${o.i.data && o.i.data.type||''}`,
-    onclick: o.onclick,
-    onRclick: o.onRclick,
-    tab: o.tab,
-    rtn: true
-  });
+  // function test(){
+  //   if(o.i.type === 'image'){}
+  // }
   if(!o.i||!o.i.data) return;
   if(o.i.type === 'image'){
     if(o.i.data.type.match(/video|gif/)){
-      new El().Video({
+      if(o.type.match(/media/)){
+        player({
+          path: o.path,
+          url: `https://leonardo.osnova.io/${o.i.data.uuid}`,
+          poster: o.poster
+        })
+      }else{
+        const main=new El().Div({
+          path: o.path,
+          cName: `mask ${o.type} ${o.i.data && o.i.data.type||''}`,
+          autoplay: true,
+          rtn: true
+        });
+        new El().Video({
+          path: main,
+          type: o.i.type,
+          cName: 'attach',
+          url: `https://leonardo.osnova.io/${o.i.data.uuid}`
+        })
+      }
+    }else{
+      const main=new El().Div({
+        path: o.path,
+        cName: `mask ${o.type} ${o.i.data && o.i.data.type||''}`,
+        onclick: o.onclick,
+        onRclick: o.onRclick,
+        tab: o.tab,
+        rtn: true
+      });
+      new El().Image({
         path: main,
         cName: 'attach',
         url: `https://leonardo.osnova.io/${o.i.data.uuid}`
       })
-    }else
-    new El().Image({
-      path: main,
-      cName: 'attach',
-      url: `https://leonardo.osnova.io/${o.i.data.uuid}`
-    })
+    }
+  }else
+  if(o.i.type === 'audio'){
+    player({
+      path: o.path,
+      type: o.i.type,
+      remove: true,
+      url: `https://leonardo.osnova.io/${o.i.data.uuid}`,
+      controls: true
+    });
+    // new El().Audio({
+    //   path: o.path,
+    //   cName: 'attach',
+    //   controls: true,
+    //   url: `https://leonardo.osnova.io/${o.i.data.uuid}`
+    // })
   }
 };
